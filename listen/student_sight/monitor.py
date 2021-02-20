d18b8aadb2790a51881c871445956178bc963486
techwizzie <sanjayipscoc@gmail.com>
2021-02-20 10:17:31 +0530

    Fixed uptime downtime issue - Ends the class if downtime is greater than 30 seconds

--- listen/student_sight/monitor.py
+++ listen/student_sight/monitor.py
@@ -3,8 +3,11 @@ from .telegram_utils import sight_bot
 import pandas as pd 
 import time
 import os 
+import xlwt
+from openpyxl import load_workbook
+from xlwt import Workbook
 import json
-
+import pandas as pd
 class colors:
     red = '\033[91m'
     green = '\33[92m'
@@ -21,9 +24,6 @@ def format_time(seconds):
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 
-def clear_terminal():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
 def start():
     f = open('usr_data.json','r')
     j = json.loads(f.read())
@@ -37,10 +37,8 @@ def start():
     class_start_time = time.time()
     total_uptime = 0
     while True:
-        try:
-            if look_for_human() == True:
-                clear_terminal()
-                print(colors.green, "Found student :)  [press ctrl + C to quit]", colors.end)
+        if look_for_human() == True:
+                bot.send_message('Recognised student :)')
                 start_time = time.time()
                 while True:
                     h = look_for_human()
@@ -48,16 +46,35 @@ def start():
                         end_time = time.time()
                         # print("uptime was :", end_time - start_time)
                         total_uptime += end_time - start_time
-                        clear_terminal()
-                        print(colors.red , "Student not found  [press ctrl + C to quit]", colors.end)
+                        bot.send_message('Student not found :(')
                         break
                     else:
-                        pass
-        except KeyboardInterrupt:
+                        pass    
+        else:
             class_end_time = time.time()
             downtime = class_end_time - class_start_time - total_uptime
+            if downtime < 30:
+                continue
             end_message = student_name + " `{" + "ID: "+ student_roll_number +  "}`" + " left the class \n`Uptime: " + str(format_time(total_uptime)) + "` \n`Downtime: " + str(format_time(downtime)) + "`"
             resp = bot.send_message(end_message)
-            clear_terminal()
+            writer = pd.ExcelWriter('attention.xlsx', engine='openpyxl')
+            writer.book = load_workbook('attention.xlsx')
+            writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
+            reader = pd.read_excel(r'attention.xlsx')
+            df = pd.DataFrame({'Username': [student_roll_number], 
+                                'Uptime': [format_time(total_uptime)],
+                                'Downtime': [format_time(downtime)]})
+            df.to_excel(writer, index=False, header=False,  startrow=len(reader)+1)
+            writer.close()
+            '''wb = Workbook()
+            sheet1 = wb.add_sheet('Attention Report')
+            style = xlwt.easyxf('font: bold 1')
+            sheet1.write(0, 0, 'Username', style)
+            sheet1.write(0, 1, 'Uptime', style)
+            sheet1.write(0, 2, 'Downtime', style)
+            sheet1.write(1, 0, student_roll_number)
+            sheet1.write(1, 1, format_time(total_uptime))
+            sheet1.write(1, 2, format_time(downtime))
+            wb.save('attention.xls')'''
             print("\nClass ended \nUptime: " , format_time(total_uptime), "\nDowntime: ", format_time(downtime))
             break
